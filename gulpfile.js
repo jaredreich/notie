6947f1155c293e7223faa30d4a01d72190e5ff85
Jared Reich <jaredreich@gmail.com>
2017-02-26 13:33:09 -0700

    Add webpack to dev/build for UMD output.

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,59 @@
+const Webpack = require('webpack')
 const gulp = require('gulp')
-const babel = require('gulp-babel')
+const webpack = require('gulp-webpack')
 const cssnano = require('gulp-cssnano')
 const rename = require('gulp-rename')
 const sass = require('gulp-sass')
-const uglify = require('gulp-uglify')
 const del = require('del')
 const run = require('run-sequence')
 
 gulp.task('clean', () => del(['./dist']))
 
+const webpackConfig = minimize => ({
+  output: {
+    filename: minimize ? 'notie.min.js' : 'notie.js',
+    library: 'notie',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  plugins: minimize
+    ? [
+      new Webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false },
+        mangle: true,
+        sourcemap: false
+      })
+    ]
+    : []
+})
+
 gulp.task('script', () => {
   gulp.src('./src/notie.js')
-    .pipe(babel({
-      plugins: [
-        'transform-es2015-modules-umd'
-      ],
-      presets: [
-        'es2015', 'stage-3'
-      ]
-    }))
-    .pipe(gulp.dest('./dist'))
-    .pipe(uglify())
-    .pipe(rename('notie.min.js'))
-    .pipe(gulp.dest('./dist'))
+  .pipe(webpack(webpackConfig(false), Webpack))
+  .pipe(gulp.dest('./dist'))
+  .pipe(webpack(webpackConfig(true), Webpack))
+  .pipe(gulp.dest('./dist'))
 })
 
 gulp.task('style', () => {
   gulp.src(['./src/notie.scss'])
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'))
-    .pipe(cssnano())
-    .pipe(rename('notie.min.css'))
-    .pipe(gulp.dest('./dist'))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./dist'))
+  .pipe(cssnano())
+  .pipe(rename('notie.min.css'))
+  .pipe(gulp.dest('./dist'))
 })
 
 gulp.task('default', ['clean'], () => {
   run('script', 'style')
-  gulp.watch('./src/notie.scss', ['style'])
-  gulp.watch('./src/notie.js', ['script'])
-})
-
-gulp.task('watch', () => {
   gulp.watch('./src/notie.scss', ['style'])
   gulp.watch('./src/notie.js', ['script'])
 })
